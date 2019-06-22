4d12b23bed6b3203f3a5e7779e2ab612fbc61293
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2019-06-22 15:57:03 -0500

    feat(build-wc): Adding dependencies for create web component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { BiometricComponent } from './biometric/biometric.component';
 import { FormsModule } from '@angular/forms';
+import { createCustomElement } from '@angular/elements';
+
+declare const customElements;
 
 @NgModule({
   declarations: [
@@ -19,15 +21,22 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  entryComponents: []
+  bootstrap: [
+    // AppComponent
+  ],
+  entryComponents: [
+    BiometricComponent
+  ]
 })
 export class AppModule {
 
-  constructor(private readonly inject: Injector) {}
+  constructor(private readonly injector: Injector) {}
 
   ngDoBootstrap(): void {
     console.log('initialize spa - application web');
+    const { injector } = this;
+    const ngCustomElement = createCustomElement(BiometricComponent, { injector });
+    customElements.define('wc-biometrics', ngCustomElement);
   }
 
 }
